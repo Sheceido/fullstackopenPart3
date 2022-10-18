271dd1607ebc8502b5477a205cfcff2eddd370a7
sheceido <leonpoon@hotmail.ca>
2022-10-18 01:33:39 -0400

    mongoDB access update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const Note = require('./models/note');
 
+const app = express();
 // json-parser to access data easily and for POST
 // Without this, the 'request.body' would be undefined
 // takes JSON data from request, transform into js object,
@@ -19,68 +22,46 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger);
-
-const cors = require('cors');
 app.use(cors());
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    },
-  ]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes);
+    Note
+        .find({})
+        .then(notes => {
+            response.json(notes);
+        });
 });
 
 app.post('/api/notes', (request, response) => {
     const body = request.body;
     
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        });
+    if (body.content === undefined) {
+        return response.status(400).json({error: 'content missing'});
     }
 
-    const note = {
-        id: generateId2(),
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-    }
+    });
 
-    notes = notes.concat(note);
-    response.json(note);
+    note
+        .save()
+        .then(savedNote => {
+            response.json(savedNote);
+        });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(n => n.id === id);
-
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    Note
+        .findById(request.params.id)
+        .then(note => {
+            response.json(note);
+        })
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -105,13 +86,13 @@ app.use(unknownEndpoint);
  * transformed into individual numbers by using the
  * "three dot" spread syntax ....
  */
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n=> n.id))
-    : 0
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n=> n.id))
+//     : 0
 
-    return maxId + 1;
-}
+//     return maxId + 1;
+// }
 
 /** Alternatively, use reduce method -
  * map returns an array, then array.reduce by
@@ -120,13 +101,13 @@ const generateId = () => {
  * 
  * this iteration does not check for event of 0 sized notes-array
 */
-const generateId2 = () => {
-    return 1 + notes
-        .map(n => n.id)
-        .reduce((prev, curr) => Math.max(prev, curr))
-}
+// const generateId2 = () => {
+//     return 1 + notes
+//         .map(n => n.id)
+//         .reduce((prev, curr) => Math.max(prev, curr))
+// }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on Port ${PORT}`);
 });
